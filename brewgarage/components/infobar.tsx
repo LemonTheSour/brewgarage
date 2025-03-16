e97e156f54e4a96e48be4696574d3cc06d4e79ce
Michael <m.m.downing@bigpond.com>
2025-03-16 11:21:52 +0800

    infobar mobile + desktop implemented

---
+++ brewgarage/components/infobar.tsx
@@ -0,0 +1,8 @@
+export default function infobar() {
+  return (
+    <div className="flex w-full h-8 bg-black items-center justify-center text-white lg:text-lg">
+      <div className="">20 Millford St, East Vic Park</div>
+      <div className="pl-15">0486 035 890</div>
+    </div>
+  );
+}
